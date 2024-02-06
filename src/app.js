6ec29eb17d679ecf591233a86dbbf77c89de8bb2
NaziaB26 <naziab2627@gmail.com>
2024-02-06 14:08:03 +0000

    added episodecodes

--- src/app.js
+++ src/app.js
@@ -4,36 +4,21 @@ const { gameOfThronesEpisodes } = require("./data/gameOfThronesData");
 
 
 
-// Removed temporary console logging
-// summariseEpisodesToConsole(gameOfThronesEpisodes);
-
-// Configure the server's route handlers
-
-const level100Array = gameOfThronesEpisodes.map((obj) => ({
-    name: obj.name,
-    season: obj.season,
-    episode: obj.number,
-    image: obj.image,
-    summary: obj.summary,
-}));
-
 app.get("/", (req, res) => {
     res.render("pages/index", {
-        episodeArray: level100Array,
-        movie: "Bring it on",
+        episodeArray: gameOfThronesEpisodes  
     });
 });
 
-app.get("/db-test", async (req, res) => {
-    try {
-        const dbResult = await query("select now()");
-        const rows = dbResult.rows;
-        res.json(rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Sorry, an error occurred on the server.");
-    }
-});
+function makeEpisodeCode(episode) {
+    const padSeason = String(episode.season).padStart(2, "0");
+    const padEpisode = String(episode.number).padStart(2, "0");
+
+    return `S${padSeason}E${padEpisode}`;
+}
+//make this function accessable in any ejs in our app.
+app.locals.makeEpisodeCode=makeEpisodeCode;
+
 
 const PORT_NUMBER = process.env.PORT ?? 3000;
 
---
+++ src/data/example.js
@@ -0,0 +1,3 @@
+const level100Array = [{name: "north"},{name: "night"},{name: "dead"}]
+
+const episodeCodes =["S02E01", "S02E02", "S02E03"]
