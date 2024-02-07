fd2d1ae78accf7aa92e607adcbd6b3b2f3aa6ae4
tomi-owoeye <tomithegr8@gmail.com>
2024-02-07 16:45:28 +0000

    level 150 started

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
 const { app } = require("./support/setupExpress");
 const { query } = require("./support/db");
-const { gameOfThronesEpisodes } = require("./data/gameOfThronesData");
+const { gameOfThronesEpisodes } = require("./data/gameOfThronesData"); // get all data from GOT database
 
 
 
 app.get("/", (req, res) => {
-    res.render("pages/index", {
+    res.render("pages/index", { 
+    });
+});
+
+app.get("/fullepisodes", (req, res) => { //cannot have spaces between nav names
+    res.render("pages/fullEpisodes", {
         episodeArray: gameOfThronesEpisodes  
     });
 });
+
+function findEpisodeById(id, array) {
+    for (let element of array) {
+        if (id === element.id) {
+            return element;
+        }
+    }
+}
+
+app.get("/singleEpisode/:episodeid", (req, res) => { //cannot have spaces between nav names
+    const episodeid = req.params.episodeid
+    // Fetch episode details based on episodeId from your data source
+     
+         const selectedEpisode = findEpisodeById(episodeid, gameOfThronesEpisodes);
+         res.render("pages/singleEpisode", {
+             episode: selectedEpisode,
+         });
+     });
+
 
 function makeEpisodeCode(episode) {
     const padSeason = String(episode.season).padStart(2, "0");
@@ -18,6 +42,16 @@ function makeEpisodeCode(episode) {
 }
 //make this function accessable in any ejs in our app.
 app.locals.makeEpisodeCode=makeEpisodeCode;
+
+
+
+// level 150- clicking onto individual episodes
+
+
+
+
+
+
 
 
 const PORT_NUMBER = process.env.PORT ?? 3000;
